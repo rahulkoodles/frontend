b4cda03e3ef05092ffbdc951fafe5af5b09134c2
Aman Singhal <aman@aman.singhal>
2024-02-22 12:04:59 +0530

    header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,35 @@ import CardCollections from "./components/CardCollections/CardCollections";
 import Sidebar from "./components/SideBar/Sidebar";
 import OfferDetails from "./components/offerDetails/OfferDetails";
 
+//********************** Wallet Connect ******************* */
+
+
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5/react'
+
 function App() {
+const projectId = '20eb0c483bb4801ec4004dea1a843576'
+
+const mainnet = {
+  chainId: 1,
+  name: 'Ethereum',
+  currency: 'ETH',
+  explorerUrl: 'https://etherscan.io',
+  rpcUrl: 'https://cloudflare-eth.com'
+}
+const metadata = {
+  name: 'My Website',
+  description: 'My Website description',
+  url: 'https://mywebsite.com', 
+  icons: ['https://avatars.mywebsite.com/']
+}
+
+createWeb3Modal({
+  ethersConfig: defaultConfig({ metadata }),
+  chains: [mainnet],
+  projectId,
+  enableAnalytics: true 
+})
+
   return (
     <div>
       <div className=" flex flex-nowrap gap-[20px]">
@@ -16,3 +44,6 @@ function App() {
 }
 
 export default App;
+
+
+
---
+++ src/EtherHelper.js
@@ -0,0 +1,192 @@
+import { ethers } from "ethers";
+import { whaleAbi } from "../src/abis/whaleAbi";
+
+export const handleWalletConnect = async () => {
+  try {
+    if (!window.ethereum)
+      throw new Error("No crypto wallet found. Please install it.");
+    await window.ethereum.send("eth_requestAccounts");
+    window.ethereum.enable();
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const signature = await signer.signMessage("Hi,from OTC");
+    const address = await signer.getAddress();
+
+    return {
+      signature,
+      address,
+    };
+  } catch (err) {
+    // console.log('err', err);
+  }
+};
+
+export const getProvider = () => {
+  if (window.ethereum || window.web3) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    return {
+      provider,
+      signer,
+    };
+  } else {
+    return false;
+  }
+};
+
+export const getContractInstance = async (
+  contractAddress,
+  contractABI,
+  signer
+) => {
+  try {
+    if (!window.ethereum) {
+      throw new Error("No crypto wallet found. Please install it.");
+    }
+
+    const contract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    return contract;
+  } catch (error) {
+    // console.log(error);
+  }
+};
+export const calculateRemainingSeconds = (timestamp) => {
+  const currentTime = Math.floor(Date.now() / 1000);
+  const endTime = parseInt(timestamp);
+
+  if (endTime > currentTime) {
+    const remainingSeconds = endTime - currentTime;
+
+    return remainingSeconds;
+  } else {
+    return 0;
+  }
+};
+
+//  *********************** Get Token Balance ********************************** */
+
+export const getTokenBalance = async (address) => {
+  const erc20 = await connectSmartContract();
+  try {
+    const balance = await erc20.balanceOf(address);
+    const decimal = await erc20.decimals();
+    const formattedBalance = ethers.utils.formatUnits(balance, decimal);
+    return formattedBalance;
+  } catch (error) {
+    console.error("An error occurred:", error);
+    return error;
+  }
+};
+
+// //   *********************** Get Token Name ********************************** */
+
+export const getTokenName = async () => {
+  const erc20 = await connectSmartContract();
+  try {
+    const name = await erc20.name();
+    return name;
+  } catch (error) {
+    console.error("An error occurred In Name:", error);
+    return error;
+  }
+};
+
+//   *********************** Get Token Symbol ********************************** */
+export const getTokenSymbol = async () => {
+  const erc20 = await connectSmartContract();
+  try {
+    const symbol = await erc20.symbol();
+    console.log("Symbol", symbol);
+    return symbol;
+  } catch (error) {
+    console.error("An error occurred In Symbol:", error);
+    return error;
+  }
+};
+
+//   *********************** Get Token TotalSupply ********************************** */
+
+export const getTokenTotalSupply = async () => {
+  const erc20 = await connectSmartContract();
+  try {
+    const decimal = await erc20.decimals();
+    const Supply = await erc20.totalSupply();
+    const formattedTotalSupply = ethers.utils.formatEther(Supply, decimal);
+    const format = formattedTotalSupply.toString();
+    return format;
+  } catch (error) {
+    console.error("An error occurred In Total Supply:", error);
+    return error;
+  }
+};
+
+//************************ Send Token Mint *********************** */
+export const sendTokenMint = async (to, amount) => {
+  const erc20 = await connectSmartContract();
+  try {
+    const decimal = await erc20.decimals();
+    const format = ethers.utils.parseUnits(amount, decimal);
+
+    const value = await erc20.mint(to, format);
+    return value;
+  } catch (error) {
+    console.error("An error occurred In Total Supply:", error);
+    return error;
+  }
+};
+
+//************************ Send Token Balance *********************** */
+export const sendTokenBalance = async (to, amount) => {
+  const erc20 = await connectSmartContract();
+  try {
+    const decimal = await erc20.decimals();
+    const format = ethers.utils.parseUnits(amount, decimal);
+    const value = await erc20.transfer(to, format);
+    return value;
+  } catch (error) {
+    console.error("Sending Money Error:", error);
+    return error;
+  }
+};
+
+//************************ Token Approve *********************** */
+export const TokenApprove = async (to, amount) => {
+  const erc20 = await connectSmartContract();
+  try {
+    const decimal = await erc20.decimals();
+    const format = ethers.utils.parseUnits(amount, decimal);
+
+    console.log("amount format", format);
+    const value = await erc20.approve(to, format);
+    // return value
+  } catch (error) {
+    console.error("Sending Approval Error:", error);
+    return error;
+  }
+};
+
+//************************ Send Token Transform *********************** */
+export const sendTokenTransform = async (from, to, amount) => {
+  const erc20 = await connectSmartContract();
+  try {
+    const decimal = await erc20.decimals();
+    const format = ethers.utils.parseUnits(amount, decimal);
+    const value = await erc20.transferFrom(from, to, format);
+    return value;
+  } catch (error) {
+    console.error("Login from Your Account Error:", error);
+    return error;
+  }
+};
+
+//*************************************************************
+
+export const connectSmartContract = async () => {
+  const result = getProvider();
+  const signer = result.signer;
+  const erc20ContractAddress = "0x060B673B58aEAb73a4851B233a00aA305BE21B72";
+  const erc20 = new ethers.Contract(erc20ContractAddress, whaleAbi, signer);
+  return erc20;
+};
