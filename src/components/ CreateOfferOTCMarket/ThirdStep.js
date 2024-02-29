4c3b64c21ca2bae94dd4da2d377513e4c5ef5beb
vishal <vishal.prajapati@oodles.io>
2024-02-29 10:32:57 +0530

    fix issue

--- src/components/ CreateOfferOTCMarket/ThirdStep.js
+++ src/components/ CreateOfferOTCMarket/ThirdStep.js
@@ -11,7 +11,7 @@ function ThirdStep({ StepsIncreament, StepsDecreament }) {
   const { formState, updateFormState, resetFormState } =
     useContext(FormDataContext);
 
-  const signer = useSigner();
+  const { signer } = useSigner();
 
   async function handleSubmit() {
     const {
@@ -33,10 +33,16 @@ function ThirdStep({ StepsIncreament, StepsDecreament }) {
     const baseTokenDecimals = await baseTokenContract.decimals();
     const quoteTokenDecimals = await quoteTokenContract.decimals();
 
-    let approveAmount = ethers.utils.parseUnits(quoteAmount, quoteTokenDecimals);
+    let approveAmount = ethers.utils.parseUnits(
+      quoteAmount,
+      quoteTokenDecimals
+    );
     approveAmount = approveAmount.add(approveAmount.div(1000));
 
-    const approveTx = await quoteTokenContract.approve(escrow9mmContract.contract.address, approveAmount);
+    const approveTx = await quoteTokenContract.approve(
+      escrow9mmContract.contract.address,
+      approveAmount
+    );
     await approveTx.wait();
 
     const createOfferTx = await escrow9mmContract.createOffer(
@@ -249,7 +255,6 @@ function ThirdStep({ StepsIncreament, StepsDecreament }) {
                         className="capitalize rounded-lg p-7   border border-black text-ct-gray-950 text-xl font-semibold disabled:cursor-not-allowed flex items-center justify-center gap-2 bg-[#87EE94] !hover:bg-[#87EE94]"
                         htmlType="submit"
                       >
-                        {' '}
                         Submit
                       </Button>
                     </div>
